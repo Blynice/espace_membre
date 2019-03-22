78cc99c8699f4bceb63eaa97222349dc38d94fd3
Blynice <33175844+Blynice@users.noreply.github.com>
2019-03-22 09:37:21 +0100

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,20 @@
+    <footer>
+      <nav>
+        <ul class="nav">
+          <li class="nav-item">
+            <i class="fab fa-github"></i>
+            <a class="nav-link text-primary" href="https://github.com/Blynice">github</a>
+          </li>
+          <li class="nav-item">
+            <i class="fab fa-codepen"></i>
+            <a class="nav-link text-primary" href="https://codepen.io/Minky/">codepen</a>
+          </li>
+          <li class="nav-item">
+            <i class="fas fa-laptop-code"></i>
+            <a class="nav-link text-primary" href="#">Link 2</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,28 @@
+<?php
+  require_once 'application/data/db.php';
+?>
+
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!--- custom css --->
+    <link rel="stylesheet" href="style/main.css">
+
+    <!--- google font ---->
+    <link href="https://fonts.googleapis.com/css?family=Lato|Signika" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
+    
+    <!--- font awesome ------>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.0/css/all.css" integrity="sha384-Mmxa0mLqhmOeaE8vgOSbKacftZcsNYDjQzuCOm6D02luYSzBG8vpaOykv9lFQ51Y" crossorigin="anonymous">
+
+    <title>Espace membre</title>
+  </head>
+  <body>
