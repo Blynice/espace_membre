391f3d5d6b273630906f96183eda7ba38ff6e3c1
Blynice <33175844+Blynice@users.noreply.github.com>
2019-04-25 15:05:29 +0200

    Add files via upload

---
+++ script/changeTheme.js
@@ -0,0 +1,135 @@
+//Change theme for user profil//
+
+// theme nature //
+
+
+var bgNature;
+var jumbotronNature;
+var fontHeaderNature;
+var fontTitleNature;
+var fontProfilNature;
+var bgCardNature;
+var footerProfilNature;
+
+bgNature = document.getElementById('bgProfilTheme');
+jumbotronNature = document.getElementById('jumbotronTheme');
+fontHeaderNature = document.getElementById('headerTitleTheme');
+fontTitleNature = document.getElementById('titleCardTheme');
+fontProfilNature = document.getElementById('fontProfilTheme');
+bgCardNature = document.getElementById('cardBgTheme');
+footerProfilNature = document.getElementById('footerTheme');
+
+
+
+function themeNature() {
+
+    bgNature.classList.toggle('bg_nature');
+    jumbotronNature.classList.toggle('jumbotron_nature');
+    fontHeaderNature.classList.toggle('font_nature');
+    fontTitleNature.classList.toggle('font_nature');
+    fontProfilNature.classList.toggle('font_nature');
+    bgCardNature.classList.toggle('bg_none');
+    footerProfilNature.classList.toggle('footer_nature');
+};
+
+nature.addEventListener('click', themeNature);
+
+
+//  theme Rubble //
+
+var bgRubble;
+var jumbotronRubble;
+var fontHeaderRubble;
+var fontTitleRubble;
+var fontProfilRubble;
+var bgCardRubble;
+var footerProfilRubble;
+
+bgRubble = document.getElementById('bgProfilTheme');
+jumbotronRubble = document.getElementById('jumbotronTheme');
+fontHeaderRubble = document.getElementById('headerTitleTheme');
+fontTitleRubble = document.getElementById('titleCardTheme');
+fontProfilRubble = document.getElementById('fontProfilTheme');
+bgCardRubble = document.getElementById('cardBgTheme');
+footerProfilRubble = document.getElementById('footerTheme');
+
+
+
+function themeRubble() {
+
+    bgRubble.classList.toggle('bg_rubble');
+    jumbotronRubble.classList.toggle('jumbotron_rubble');
+    fontHeaderRubble.classList.toggle('font_rubble');
+    fontTitleRubble.classList.toggle('font_rubble');
+    fontProfilRubble.classList.toggle('font_rubble');
+    bgCardRubble.classList.toggle('bg_none');
+    footerProfilRubble.classList.toggle('footer_rubble');
+};
+
+rubble.addEventListener('click', themeRubble);
+
+//  theme PJmask //
+
+var bgPJmask;
+var jumbotronPJmask;
+var fontHeaderPJmask;
+var fontTitlePJmask;
+var fontProfilPJmask;
+var bgCardPJmask;
+var footerProfilPJmask;
+
+bgPJmask = document.getElementById('bgProfilTheme');
+jumbotronPJmask = document.getElementById('jumbotronTheme');
+fontHeaderPJmask = document.getElementById('headerTitleTheme');
+fontTitlePJmask = document.getElementById('titleCardTheme');
+fontProfilPJmask = document.getElementById('fontProfilTheme');
+bgCardPJmask = document.getElementById('cardBgTheme');
+footerProfilPJmask = document.getElementById('footerTheme');
+
+
+
+function themePJmask() {
+
+    bgPJmask.classList.toggle('bg_PJmask');
+    jumbotronPJmask.classList.toggle('jumbotron_PJmask');
+    fontHeaderPJmask.classList.toggle('font_PJmask');
+    fontTitlePJmask.classList.toggle('font_PJmask');
+    fontProfilPJmask.classList.toggle('font_PJmask');
+    bgCardPJmask.classList.toggle('bg_none');
+    footerProfilPJmask.classList.toggle('footer_PJmask');
+};
+
+PJmask.addEventListener('click', themePJmask);
+
+
+
+// theme Night //
+
+
+var bgNight;
+var jumbotronNight;
+var fontHeaderNight;
+var fontTitleNight;
+var bgCardPJmask;
+var fontProfilNight;
+
+bgNight = document.getElementById('bgProfilTheme');
+jumbotronNight = document.getElementById('jumbotronTheme');
+fontHeaderNight = document.getElementById('headerTitleTheme');
+fontTitleNight = document.getElementById('titleCardTheme');
+bgCardNight = document.getElementById('cardBgTheme');
+fontProfilNight = document.getElementById('fontProfilTheme');
+
+
+
+function themeNight() {
+
+    bgNight.classList.toggle('bg_night');
+    jumbotronNight.classList.toggle('jumbotron_night');
+    fontHeaderNight.classList.toggle('font_night');
+    fontTitleNight.classList.toggle('font_night');
+    bgCardNight.classList.toggle('bg_none');
+    fontProfilNight.classList.toggle('font_night');
+};
+
+night.addEventListener('click', themeNight);
--- script/script.js
+++ script/script.js
@@ -1,22 +1,26 @@
-// changement du theme de la page principale 
+// changement du theme de la page principale
 
 var colorBackground;
 var colorJumbotron;
 var colorButton;
 var titleFont;
+var btnFont;
 
+//Récupération des id
 colorBackground = document.getElementById('bg');
 colorJumbotron = document.getElementById('jumbotron');
 colorButton = document.getElementById('btn');
 titleFont = document.getElementById('font');
+btnFont = document.getElementById('btn');
 
-
+//Function changer thème
 function changeTheme() {
 
     colorBackground.classList.toggle('bg-green');
     colorJumbotron.classList.toggle('green');
     colorButton.classList.toggle('btn-green');
-    titleFont.classList.toggle('font-title');
+    titleFont.classList.toggle('font-change');
+    btnFont.classList.toggle('font-change');
 };
 
 
