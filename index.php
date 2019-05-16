1535516c6569e99542148d62c9b41ce324c72d93
Blynice <33175844+Blynice@users.noreply.github.com>
2019-05-16 10:05:10 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -16,30 +16,32 @@ require 'application/reg.php';
     <div class="container container-color" id="bg">
         <div class ="d-flex justify-content-center">
 
-        <form action="index.php" method="post" class="form-signin">
-            <label for="pseudo" class="sr-only">Pseudo</label>
-            <input type="text"  name= "pseudo" class="form-control" placeholder="Pseudo" required autofocus>
+          <form action="index.php" method="post" class="form-signin">
+              <label for="pseudo" class="sr-only">Pseudo</label>
+              <input type="text"  name= "pseudo" class="form-control" placeholder="Pseudo" required autofocus
+                  value ="<?php if (isset($_POST["pseudo"])) echo $_POST["pseudo"]?>">
 
-            <label for="city" class="sr-only">Votre ville</label>
-            <input type="text"  name= "city" class="form-control" placeholder="Votre ville" required autofocus>
+              <label for="city" class="sr-only">Votre ville</label>
+              <input type="text"  name= "city" class="form-control" placeholder="Votre ville" required autofocus
+              value ="<?php if (isset($_POST["city"])) echo $_POST["city"]?>">
 
-            <label for="email" class="sr-only">Votre email</label>
-            <input type="email"  name= "email" class="form-control" placeholder="Votre email" required autofocus>
+              <label for="email" class="sr-only">Votre email</label>
+              <input type="email"  name= "email" class="form-control" placeholder="Votre email" required autofocus
+              value ="<?php if (isset($_POST["email"])) echo $_POST["email"]?>">
 
+              <label for="password" class="sr-only">Mot de passe</label>
+              <input type="password"  name="password" class="form-control" placeholder="Mot de passe" required>
 
-            <label for="password" class="sr-only">Mot de passe</label>
-            <input type="password"  name="password" class="form-control" placeholder="Mot de passe" required>
+              <label for="password" class="sr-only">Mot de passe(confirmer)</label>
+              <input type="password"  name="password_confirm" class="form-control" placeholder="Mot de passe(confirmer)" required>
 
-            <label for="password" class="sr-only">Mot de passe(confirmer)</label>
-            <input type="password"  name="password_confirm" class="form-control" placeholder="Mot de passe(confirmer)" required>
+              <button id="btn" name="inscription" class="btn btn-lg btn-primary btn-block" type="submit">Je m'inscris</button>
+              <?php
+                  if(isset($error)){
+                   echo '<div class="error text-center">', $error, '</div>';  }
+              ?>
 
-            <button id="btn" name="inscription" class="btn btn-lg btn-primary btn-block" type="submit">Je m'inscris</button>
-        </form>
-
-<?php
-  if(isset($error)){
-      echo '<div class="error text-center">', $error, '</div>';  }
-?>
+          </form>
         </div>
     </div>
 
