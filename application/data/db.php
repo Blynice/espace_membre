5a540ead175f2ff669f59fe634ad0538ab6b30e8
Blynice <33175844+Blynice@users.noreply.github.com>
2019-06-13 09:46:15 +0200

    Update db.php

--- application/data/db.php
+++ application/data/db.php
@@ -1,7 +1,6 @@
 <?php
 /***************************************Connexion à la base de données*******************************/
 
-  /*  $db = new PDO('mysql:host=localhost;dbname=brce8142_espace_membre;charset=utf8', 'brce8142_selyne', 'Kobayashi2019');*/
 try
 {
   $db = new PDO('mysql:host=localhost;dbname=espace_membre;charset=utf8', 'root', '');
